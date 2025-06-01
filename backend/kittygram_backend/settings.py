c4a66af3e2b7525c2f24981368054e31e646c53d
Vladimir Suvorov <qprym@ya.ru>
2025-06-01 14:40:42 +0300

    fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'qprym-kitty.sytes.net', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'qprym-kittygram.sytes.net', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
